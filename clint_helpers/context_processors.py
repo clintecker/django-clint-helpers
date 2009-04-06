d67737d8f4c4ff6f1dab0e077bf3cfb220e354a9
Clinton Ecker <clint@arstechnica.com>
2009-04-05 21:21:46 -0500

    Adding in context processor and making setup.py work

---
+++ clint_helpers/context_processors.py
@@ -0,0 +1,15 @@
+"""
+Request Processors I find useful
+"""
+
+from django.conf import settings
+from django.contrib.sites.models import Site
+
+def site(request):
+    """
+    Returns context variables required by apps that use Django's Sites app.
+    """
+    current_site = Site.get_current()
+    return {
+        'site': current_site,
+    }
---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='clint_helpers',
+      version='0.1',
+      description='A little library of things that I keep having to recreate in Django apps',
+      author='Clint Ecker',
+      author_email='me@clintecker.com',
+      url='http://github.com/clintecker/django-clint-helpers/tree/master',
+      packages=['clint_helpers',],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+      )
