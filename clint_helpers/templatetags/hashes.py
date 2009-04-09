4c09f9e3b5f5c56989d51bc7db05fb37f51bced8
Clinton Ecker <clint@arstechnica.com>
2009-04-09 10:09:35 -0500

    hashlib templates

--- clint_helpers/templatetags/hashes.py
+++ clint_helpers/templatetags/hashes.py
@@ -1,28 +1,29 @@
 from django import template
 try:
-  from hashlib import md5
-  from hashlib import sha1
-  hashlib = True
+  from hashlib import md5 as gen_md5
+  from hashlib import sha1 as gen_sha
+  hlib = True
 except:
-  import md5, sha
-  hashlib = False
+  import md5 as gen_md5
+  import sha as gen_sha
+  hlib = False
  
 register = template.Library()
  
 @register.filter
 def md5(value):
-    "Returns the hex digest of an MD5 hash of a string"
-    if hashlib:
-      h = md5(value)
-    else:
-      h = md5.new(value)
-    return h.hexdigest()
+  "Returns the hex digest of an MD5 hash of a string"
+  if hlib:
+    h = gen_md5(value)
+  else:
+    h = gen_md5.new(value)
+  return h.hexdigest()
     
 @register.filter
-def sha1(value):
-    "Returns the hex digest of an SHA-1 hash of a string"
-    if hashlib:
-      h = sha1(value)
-    else:
-      h = sha.new(value)
-    return h.hexdigest()
+def gen_sha1(value):
+  "Returns the hex digest of an SHA-1 hash of a string"
+  if hlib:
+    h = gen_sha(value)
+  else:
+    h = gen_sha.new(value)
+  return h.hexdigest()
