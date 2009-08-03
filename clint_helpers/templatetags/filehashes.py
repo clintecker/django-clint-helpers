62b33e4c5984f746866e5c759211dd4fd8f92294
Clinton Ecker <clint@arstechnica.com>
2009-08-02 20:55:45 -0500

    Oops, real version ;)

--- clint_helpers/templatetags/filehashes.py
+++ clint_helpers/templatetags/filehashes.py
@@ -13,8 +13,6 @@ except:
   import sha
  
 def assetmd5(value):
-    
-    
     if hashlib:
       h = md5(value)
     else:
@@ -25,8 +23,19 @@ class MediaFileHashNode(template.Node):
     def __init__(self, file_path):
         self.file_path = file_path
     def render(self, context):
-        full_path = settings.MEDIA_ROOT + file_path
-        print full_path
+        full_path = settings.MEDIA_ROOT + self.file_path
+        if os.path.exists(full_path):
+            value = file(full_path).read()
+            if hashlib:
+              h = md5(value)
+            else:
+              h = md5.new(value)
+            h = h.hexdigest()
+            versioned_file = settings.MEDIA_ROOT + self.file_path.replace('.', '.%s.' % (h,))
+            versioned_resource = settings.MEDIA_URL + self.file_path.replace('.', '.%s.' % (h,))
+            if os.path.exists(versioned_file):
+                return versioned_resource
+        return settings.MEDIA_URL + self.file_path
 
 def do_mediafilehash(parser, token):
     try:
@@ -34,8 +43,8 @@ def do_mediafilehash(parser, token):
         tag_name, file_path = token.split_contents()
     except ValueError:
         raise template.TemplateSyntaxError, "%r tag requires a single argument" % token.contents.split()[0]
-    if not (format_string[0] == format_string[-1] and format_string[0] in ('"', "'")):
+    if not (file_path[0] == file_path[-1] and file_path[0] in ('"', "'")):
         raise template.TemplateSyntaxError, "%r tag's argument should be in quotes" % tag_name
     return MediaFileHashNode(file_path[1:-1])
 
-register.filter('mediafilehash', assetmd5)
+register.tag('media_file_hash', do_mediafilehash)
